8e6cb76fdd9caa77d9c51c9b581f690b5c27bbaa
mhenadhy <mhenadhyesperanca@gmail.com>
2024-02-25 11:59:28 -0300

    Criptografia e Descriptografia pronta

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const input = document.getElementById('texto')
+const input = document.querySelector('#input');
+const outinput = document.querySelector('#out-input');
+
+function criptografar(){
+   let texto = input.value;
+   let resuldCripto = texto.replace(/e/g, 'ente').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+
+   document.getElementById('out-input').innerHTML = '<section id="out-input">' + resuldCripto + '<button class="btn-copiar" onclick="copytext()">Copiar</button>' + '</section>' ;
+}
+
+function descriptografar(){
+    let texto = input.value;
+    let resulDescripto = texto.replace(/ente/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+
+    document.getElementById('out-input').innerHTML = '<section id="out-input">' + resulDescripto + '<button class="btn-copiar" onclick="copytext()">Copiar</button>' + '</section>' + '</section>';
+}
 
 function copytext(){
-    navigator.clipboard.writeText(input.value).then(() => {
+    navigator.clipboard.writeText(out-input.value).then(() => {
         alert('copied to clipboard')
+        outinput.value = ("");
     })
 }
