0c7a1ddba95d875aea04b03a434c33005a12215d
mhenadhy <mhenadhyesperanca@gmail.com>
2024-03-01 20:19:15 -0300

    botao copiar funcionando

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const input = document.querySelector('#input');
 const outinput = document.querySelector('#out-input');
 
-function criptografar(){
-   let texto = input.value;
-   let resuldCripto = texto.replace(/e/g, 'ente').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+function criptografar() {
+    let texto = input.value;
+    let resuldCripto = texto.replace(/e/g, 'ente').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
 
-   document.getElementById('out-input').innerHTML = '<section id="out-input">' + resuldCripto + '<button class="btn-copiar" onclick="copytext()">Copiar</button>' + '</section>' ;
+    document.getElementById('out-input').innerHTML = '<section id="out-input">' + resuldCripto + '<button class="btn-copiar" onclick="copytext()">Copiar</button>' + '</section>';
+    outinput.value = resuldCripto
 }
 
-function descriptografar(){
+function descriptografar() {
     let texto = input.value;
     let resulDescripto = texto.replace(/ente/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+    
+    document.getElementById('out-input').innerHTML = '<section id="out-input">' + resulDescripto + '<button class="btn-copiar" onclick="copytext()">Copiar</button>' + '</section>';
 
-    document.getElementById('out-input').innerHTML = '<section id="out-input">' + resulDescripto + '<button class="btn-copiar" onclick="copytext()">Copiar</button>' + '</section>' + '</section>';
+    outinput.value = resulDescripto
 }
 
-function copytext(){
-    navigator.clipboard.writeText(out-input.value).then(() => {
-        alert('copied to clipboard')
-        outinput.value = ("");
+
+function copytext() {
+    navigator.clipboard.writeText(outinput.value).then(() =>{
+        alert('Texto copiado')
     })
 }
